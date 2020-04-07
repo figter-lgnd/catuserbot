26424c1a20913d0d78c4de0c796f51f3addcc123
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-08 02:09:01 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -1,28 +1,28 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot help command """
 
-from userbot import CMD_HELP
-from userbot.utils import register
 
+from userbot import CMD_LIST
+from userbot.events import register
 
 @register(outgoing=True, pattern="^.about(?: |$)(.*)")
-async def help(event):
+async def helper(event):
     """ For .help command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        args = event.pattern_match.group(1)
+        if args:
+            if args in CMD_LIST:
+                await event.edit(str(CMD_LIST[args]))
+            else:
+                await event.edit("Please specify a valid module name.")
         else:
-            await event.edit("Please specify a valid module name.")
-    else:
-        await event.edit("Please specify which module do you want help for !!\
-            \nUsage: .help <module name>")
-        string = ""
-        for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "`\n"
-        await event.reply(string)
+            await event.edit("Please specify which module do you want help for!")
+            string = ""
+            for i in CMD_LIST:
+                string += str(i)
+                string += "\n"
+            await event.reply(string)
