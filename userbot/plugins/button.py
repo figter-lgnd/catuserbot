2f75eae5466d01c79685cdc7863feb78cd02f0c2
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-08 20:57:34 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -10,7 +10,7 @@ from userbot.uniborgConfig import Config
 BTN_URL_REGEX = re.compile(r"(\{([^\[]+?)\}\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 
 
-@borg.on(admin_cmd(pattern="cbutton"))  # pylint:disable=E0602
+@borg.on(events.NewMessage(pattern=r"\.cbutton(.*)", outgoing=True))
 async def _(event):
     if Config.TG_BOT_USER_NAME_BF_HER is None or tgbot is None:
         await event.edit("need to set up a @BotFather bot for this module to work")
