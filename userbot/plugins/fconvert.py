99f8629a44a07f430fb8c2fd5249a0a42dad964c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-08 23:35:04 +0530

    Update fconvert.py

--- userbot/plugins/fconvert.py
+++ userbot/plugins/fconvert.py
@@ -25,9 +25,9 @@ async def _(event):
         downloaded_file_name = await borg.download_media(
             reply_message,
             Config.TMP_DOWNLOAD_DIRECTORY,
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, event, c_time, "trying to download")
-            )
+         #   progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+          #      progress(d, t, event, c_time, "trying to download")
+           # )
         )
     except Exception as e:  # pylint:disable=C0103,W0703
         await event.edit(str(e))
