74954f245de0af705c7d3f4a9f00706a55c97e15
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-09 02:45:35 +0530

    Update selfdestruct.py

--- userbot/plugins/selfdestruct.py
+++ userbot/plugins/selfdestruct.py
@@ -13,7 +13,23 @@ import importlib.util
 
 
 
-@borg.on(admin_cmd("sd", outgoing=True  ))
+@borg.on(admin_cmd("sdm", outgoing=True  ))
+async def selfdestruct(destroy):
+    """ For .sd command, make seflf-destructable messages. """
+    if not destroy.text[0].isalpha() and destroy.text[0] not in ("/", "#", "@", "!"):
+        message = destroy.text
+        counter = int(message[4:6])
+        text = str(destroy.text[6:])
+        text = (
+            text
+        )
+        await destroy.delete()
+        smsg = await destroy.client.send_message(destroy.chat_id, text)
+        time.sleep(counter)
+        await smsg.delete()
+
+        
+@borg.on(admin_cmd("sdn", outgoing=True  ))
 async def selfdestruct(destroy):
     """ For .sd command, make seflf-destructable messages. """
     if not destroy.text[0].isalpha() and destroy.text[0] not in ("/", "#", "@", "!"):
@@ -29,4 +45,4 @@ async def selfdestruct(destroy):
         await destroy.delete()
         smsg = await destroy.client.send_message(destroy.chat_id, text)
         time.sleep(counter)
-        await smsg.delete()
+        await smsg.delete()        
