192616bd22b57a9da2789a8ac9a791c55bd7839c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-09 22:47:38 +0530

    Update qoutly.py

--- userbot/plugins/qoutly.py
+++ userbot/plugins/qoutly.py
@@ -1,38 +1,49 @@
 """QuotLy: Avaible commands: .qbot
 """
+#New Qoute module by @PhycoNinja13b 😉
+#Uses another bot named @es3n1n_bot
+# .quote for making quote
+
 import datetime
+import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd
+from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern="qbot ?(.*)"))
+@borg.on(admin_cmd(pattern="qbot ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.reply("```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await event.reply("```Reply to text message```")
        return
-    chat = "@QuotLyBot"
-    sender = reply_message.sender
+    chat = "@es3n1n_bot"
+    sender = reply_message.sender 
+
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.reply("```Reply to actual users message.```")
        return
-    await event.edit("```Making a Quote```")
+
     async with event.client.conversation(chat) as conv:
           try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=810547723))
+              message = await event.client.forward_messages(chat, reply_message)
+              await message.reply("/quote")
+
+              await asyncio.sleep(4)
+              response = await response
+
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
+              await event.reply("```Please unblock [me](https://t.me/es3n1n_bot u Nigga```")
               return
+
           if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+             await event.reply("```Can you kindly disable your forward privacy settings for good?```")
           else: 
              await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+             await event.client.send_message(event.chat_id, response.message, reply_to=event.message.reply_to_msg_id)
