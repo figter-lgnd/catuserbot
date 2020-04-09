33e66c7248ee876eba5c9604f96c879006766476
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-10 02:49:02 +0530

    Update scrapers.py

--- userbot/plugins/scrapers.py
+++ userbot/plugins/scrapers.py
@@ -40,7 +40,7 @@ from asyncio import sleep
 from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID, YOUTUBE_API_KEY, CHROME_DRIVER, GOOGLE_CHROME_BIN
 from userbot.events import register
 from telethon.tl.types import DocumentAttributeAudio
-from userbot.plugins.upload_download import progress, humanbytes, time_formatter
+from userbot.uniborgutil import progress, humanbytes, time_formatter
 from userbot.uniborgConfig import Config
 CARBONLANG = "auto"
 TTS_LANG = "en"
