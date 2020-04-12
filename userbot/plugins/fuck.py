6bf4c18da401d70249797da8e0575f97eb73ddbf
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-12 15:49:41 +0530

    Update fuck.py

--- userbot/plugins/fuck.py
+++ userbot/plugins/fuck.py
@@ -17,10 +17,8 @@ from userbot.utils import admin_cmd
 
 
 
-
-#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 @borg.on(admin_cmd(pattern="fuk"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
@@ -56,9 +54,8 @@ async def _(event):
             await event.edit(animation_chars[i % 4])
 
 
-#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 @borg.on(admin_cmd(pattern="sex"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
@@ -114,10 +111,8 @@ import asyncio
 
 
 
-
-#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 @borg.on(admin_cmd(pattern="kiss"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
@@ -130,7 +125,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "kess":
+    if input_str == "kiss":
 
         await event.edit(input_str)
 
