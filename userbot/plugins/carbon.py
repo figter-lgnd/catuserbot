da704605c8011619145551707206ca4f53c9b184
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-12 20:50:43 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -104,7 +104,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption=f"<< `Here's your carbon!` \n **Carbonised by** {DEFAULTUSER}.>>\n__The colour scheme or this carbon style is__ `{}`".format(color_name),
+         caption=f"`Here's your carbon!`\n Carbonised by {DEFAULTUSER}.\n__The colour scheme or this carbon style is__ `{}`".format(color_name),
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
