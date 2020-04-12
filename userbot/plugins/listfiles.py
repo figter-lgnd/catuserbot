98a1d96e57cdea8016e512b35179d4b21d381121
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-12 16:15:23 +0530

    Update listfiles.py

--- userbot/plugins/listfiles.py
+++ userbot/plugins/listfiles.py
@@ -23,7 +23,41 @@ if not os.path.isdir("./SAVED"):
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
      os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
 
-@borg.on(events.NewMessage(pattern=r"\.ls", outgoing=True))
+@borg.on(admin_cmd(pattern="ls ?(.*)", allow_sudo=True))
+async def lst(event):
+	if event.fwd_from:
+		return
+	input_str = event.pattern_match.group(1)
+	if input_str:
+		msg = "**Files in {} :**\n".format(input_str)
+		files = os.listdir(input_str)
+	else:
+		msg = "**Files in Current Directory :**\n"
+		files = os.listdir(os.getcwd())
+	for file in files:
+		msg += "`{}`\n".format(file)
+	if len(msg) <= Config.MAX_MESSAGE_SIZE_LIMIT:
+		await event.edit(msg)
+	else:
+		msg = msg.replace("`","")
+		out = 'filesList.txt'
+		with open(out,'w') as f:
+			f.write(f)
+		await borg.send_file(
+				event.chat_id,
+				out,
+				force_document=True,
+				allow_cache=False,
+				caption="`Output is huge. Sending as a file...`"
+		)
+		await event.delete()	
+
+			
+
+			
+
+		
+@borg.on(events.NewMessage(pattern=r"\.ls ", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -63,39 +97,3 @@ async def _(event):
 #        await event.edit("Unknown Command")
 
 
-@borg.on(admin_cmd(pattern="ls ?(.*)", allow_sudo=True))
-async def lst(event):
-	if event.fwd_from:
-		return
-	input_str = event.pattern_match.group(1)
-	if input_str:
-		msg = "**Files in {} :**\n".format(input_str)
-		files = os.listdir(input_str)
-	else:
-		msg = "**Files in Current Directory :**\n"
-		files = os.listdir(os.getcwd())
-	for file in files:
-		msg += "`{}`\n".format(file)
-	if len(msg) <= Config.MAX_MESSAGE_SIZE_LIMIT:
-		await event.edit(msg)
-	else:
-		msg = msg.replace("`","")
-		out = 'filesList.txt'
-		with open(out,'w') as f:
-			f.write(f)
-		await borg.send_file(
-				event.chat_id,
-				out,
-				force_document=True,
-				allow_cache=False,
-				caption="`Output is huge. Sending as a file...`"
-		)
-		await event.delete()	
-
-			
-
-			
-
-				
-
-        
