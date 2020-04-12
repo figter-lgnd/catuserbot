ea649f6790d192a11ee22f256b9f6822c7b6e446
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-12 15:48:07 +0530

    Update loveu.py

--- userbot/plugins/loveu.py
+++ userbot/plugins/loveu.py
@@ -9,10 +9,9 @@ from telethon import events
 
 import asyncio
 from userbot.utils import admin_cmd 
-  
-#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 @borg.on(admin_cmd(pattern="loveu"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
