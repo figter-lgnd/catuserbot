cd6d1acaa65bd6c73f68051b90df2ad3474958c7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-15 22:07:46 +0530

    Update scrapers.py

--- userbot/plugins/scrapers.py
+++ userbot/plugins/scrapers.py
@@ -116,7 +116,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="`Here's your carbon!` \n **Carbonised by** cat\n**Colour Scheme: **`{}`".format(color_name),
+         caption="<< `Here's your carbon!` \n **Carbonised by** [cat](https://github.com/sandy1709/catuserbot)>>\n**Colour Scheme: **`{}`".format(color_name),
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
