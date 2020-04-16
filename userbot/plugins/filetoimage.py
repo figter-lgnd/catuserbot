32271ae32f905ad0d803ff05a1e29c707cf1cade
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-16 13:02:03 +0530

    Update filetoimage.py

--- userbot/plugins/filetoimage.py
+++ userbot/plugins/filetoimage.py
@@ -1,6 +1,8 @@
-"""COMMAND : .z"""
+"""COMMAND : .ftoimg 
+here file must be in image file """
 from io import BytesIO
 from userbot import utils
+import asyncio
 from telethon import types
 from telethon.errors import PhotoInvalidDimensionsError
 from telethon.tl.functions.messages import SendMediaRequest
@@ -11,6 +13,7 @@ from telethon.tl.functions.messages import SendMediaRequest
 @borg.on(utils.admin_cmd(pattern=r"ftoimg"))
 async def on_file_to_photo(event):
     await event.edit("processing.....")
+    await asyncio.sleep(2)
     target = await event.get_reply_message()
     try:
         image = target.media.document
